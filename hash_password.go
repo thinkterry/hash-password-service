16940d0fa530eb581f34b209cac11e7f4b63e4d5
thinkterry <thinkterry@users.noreply.github.com>
2017-05-17 23:39:14 -0600

    Fleshed out routes and started hashing and encoding the entire HTTP body.

--- hash_password.go
+++ hash_password.go
@@ -4,6 +4,8 @@ import (
 	"crypto/sha512"
 	"encoding/base64"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -21,13 +23,22 @@ func EncodedHash(msg string) string {
 	return Base64(Hash([]byte(msg)))
 }
 
-// per https://golang.org/doc/articles/wiki/
-
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	password := string(data)
+	fmt.Fprint(w, EncodedHash(password))
+}
+
+func shutdownHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Graceful shutdown.")
 }
 
 func main() {
+	// per https://golang.org/pkg/net/http/
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/shutdown", shutdownHandler)
 	http.ListenAndServe(":8080", nil)
 }
